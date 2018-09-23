4cd57fd48140686438ab956e895073f1b02d3f4c
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-23 20:15:36 +0300

    solution for additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stackBrackets = [];
+  var openingB = [], closingB = [];
+
+  if (str.length % 2 != 0){
+    return false;
+  }
+  //create arrays for opening and closing brackets separately
+  for (var i=0; i<bracketsConfig.length; i++){
+      openingB.push(bracketsConfig[i][0]);
+      closingB.push(bracketsConfig[i][1]);
+  }
+
+  for (var j=0; j < str.length; j++){
+    for (var k=0; k < openingB.length; k++){
+      if (str[j] == openingB[k] && openingB[k] == closingB[k]){  //if brackets are the same
+        if(str[j] == stackBrackets[stackBrackets.length-1]){    
+          stackBrackets.pop();
+        }
+        else {
+          stackBrackets.push(str[j]);
+        }
+      }
+      else if(str[j] == openingB[k]){
+        stackBrackets.push(str[j]);
+        }
+      else if (str[j] == closingB[k]){
+        if (stackBrackets.length == 0 || stackBrackets.pop() != openingB[k]) {
+          return false;
+        }
+      }
+    }
+  }
+    
+  if (stackBrackets.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
